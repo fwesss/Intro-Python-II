e5179b5ebd221818e33f048e3805a09399d44ab4
Westley Feller <feller.westley@gmail.com>
2020-07-14 19:54:11 -0700

    Player class setup

--- src/player.py
+++ src/player.py
@@ -1,2 +1,25 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
+from room import Room
+
+
+class Player:
+    def __init__(self, name: str, current_room: Room):
+        self.name = name
+        self.current_room = current_room
+
+    def move(self, direction, game):
+        def commit():
+            directions = {
+                "north": self.current_room.n_to,
+                "east": self.current_room.e_to,
+                "south": self.current_room.s_to,
+                "west": self.current_room.w_to,
+            }
+
+            if directions[direction] is not None:
+                self.current_room = directions[direction]
+                game.valid_move = True
+            else:
+                print(f"\nThere is nothing {direction} of you.\n")
+                game.valid_move = False
+
+        return commit
